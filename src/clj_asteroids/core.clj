90863e1f3a6ebfa1af54f7fe76bc4ba7306d63b4
Trystan Spangler <trystan.s@comcast.net>
2014-05-17 09:05:07 -0700

    add thrust and fix rotation direction

--- src/clj_asteroids/core.clj
+++ src/clj_asteroids/core.clj
@@ -20,7 +20,7 @@
 
 
 (def game-atom (atom { :player { :x 200 :y 100
-                                 :vx 0.0 :vy 0.0 :va 0.0
+                                 :vx 0.0 :vy 0.0 :va 0.0 :thrust 0
                                  :w 32 :h 32
                                  :image "player.png"
                                  :angle (/ Math/PI 4) }}))
@@ -29,6 +29,8 @@
 
 (defn update-entity [entity]
   (-> entity
+      (update-in [:vx] #(+ % (* (:thrust entity) (Math/cos (:angle entity)))))
+      (update-in [:vy] #(+ % (* (:thrust entity) (Math/sin (:angle entity)))))
       (update-in [:x] #(+ % (:vx entity)))
       (update-in [:y] #(+ % (:vy entity)))
       (update-in [:angle] #(+ % (:va entity)))))
@@ -53,14 +55,16 @@
 
 (defn key-pressed []
   (case (key-as-keyword)
-    :a   (swap! game-atom (fn [game] (update-in game [:player :va] #(+ % 0.1))))
-    :d   (swap! game-atom (fn [game] (update-in game [:player :va] #(- % 0.1))))
+    :a   (swap! game-atom (fn [game] (update-in game [:player :va] #(- % 0.1))))
+    :d   (swap! game-atom (fn [game] (update-in game [:player :va] #(+ % 0.1))))
+    :w   (swap! game-atom (fn [game] (update-in game [:player :thrust] #(+ % 0.1))))
          (println "Unexpected key press" (key-as-keyword))))
 
 (defn key-released []
   (case (key-as-keyword)
-    :a   (swap! game-atom (fn [game] (update-in game [:player :va] #(- % 0.1))))
-    :d   (swap! game-atom (fn [game] (update-in game [:player :va] #(+ % 0.1))))
+    :a   (swap! game-atom (fn [game] (update-in game [:player :va] #(+ % 0.1))))
+    :d   (swap! game-atom (fn [game] (update-in game [:player :va] #(- % 0.1))))
+    :w   (swap! game-atom (fn [game] (update-in game [:player :thrust] #(- % 0.1))))
          (println "Unexpected key release" (key-as-keyword))))
 
 
