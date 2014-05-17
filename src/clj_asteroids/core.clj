df74b3b3b47495c239b061a763e09e8fe53ce8ea
Trystan Spangler <trystan.s@comcast.net>
2014-05-17 08:57:41 -0700

    use [a] and [d] to rotate

--- src/clj_asteroids/core.clj
+++ src/clj_asteroids/core.clj
@@ -20,7 +20,7 @@
 
 
 (def game-atom (atom { :player { :x 200 :y 100
-                                 :vx 0.2 :vy 0.0 :va 0.1
+                                 :vx 0.0 :vy 0.0 :va 0.0
                                  :image "player.png"
                                  :angle (/ Math/PI 4) }}))
 
@@ -49,11 +49,26 @@
       (with-rotation [(:angle obj)]
         (image (get-image (:image obj)) 0 0 32 32)))))
 
+(defn key-pressed []
+  (case (key-as-keyword)
+    :a   (swap! game-atom (fn [game] (update-in game [:player :va] #(+ % 0.1))))
+    :d   (swap! game-atom (fn [game] (update-in game [:player :va] #(- % 0.1))))
+         (println "Unexpected key press" (key-as-keyword))))
+
+(defn key-released []
+  (case (key-as-keyword)
+    :a   (swap! game-atom (fn [game] (update-in game [:player :va] #(- % 0.1))))
+    :d   (swap! game-atom (fn [game] (update-in game [:player :va] #(+ % 0.1))))
+         (println "Unexpected key release" (key-as-keyword))))
+
+
 (defn -main [& args]
   (defsketch example
     :title "Asteroids"
     :setup setup
     :draw draw
-    :size [600 500]))
+    :size [600 500]
+    :key-pressed key-pressed
+    :key-released key-released))
 
 ; (-main)
