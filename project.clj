2e431c56ac150c2cbc9ae66f57ffe786b5616e9d
Trystan Spangler <trystan.s@comcast.net>
2014-05-19 20:30:37 -0700

    bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-asteroids "0.1.0-SNAPSHOT"
+(defproject clj-asteroids "1.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
