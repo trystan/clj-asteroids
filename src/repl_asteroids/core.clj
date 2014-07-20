d22fb211039f4b4b32205e1e9ba3323fd13889de
Trystan Spangler <trystan.s@comcast.net>
2014-07-20 13:39:38 -0700

    setup without fun-mode

--- src/repl_asteroids/core.clj
+++ src/repl_asteroids/core.clj
@@ -1,39 +1,36 @@
 (ns repl-asteroids.core
   (:require [quil.core :refer :all]
-            [quil.middleware :refer :all]
             [repl-asteroids.quil-helpers :refer :all]))
 
 (def window-width 800)
 (def window-height 500)
 
-(def initial-state {})
+(def game-state-atom (atom {}))
 
 (defn setup []
   (smooth)
   (frame-rate 30)
-  (image-mode :center)
-  initial-state)
+  (image-mode :center))
 
 (defn update [state]
   state)
 
-(defn draw [state]
+(defn draw []
+  (swap! game-state-atom update)
   (background 4 4 16)
   (text (str (int (current-frame-rate)) " fps") 10 20))
 
-(defn key-pressed [state event]
-  state)
+(defn key-pressed []
+  nil)
 
-(defn key-released [state]
-  state)
+(defn key-released []
+  nil)
 
 (defn main []
   (defsketch demo
     :title "Asteroids"
     :size [window-width window-height]
     :setup setup
-    :update update
     :draw draw
     :key-pressed key-pressed
-    :key-released key-released
-    :middleware [fun-mode]))
+    :key-released key-released))
